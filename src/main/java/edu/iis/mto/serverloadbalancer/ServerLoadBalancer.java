d7a0dc90c15d9fbc54e544bbadf0dd6108cc582d
Patryk Wierzyński <p.wierzynski@live.com>
2017-06-13 18:20:13 +0200

    test1 - balancing 0 vms implemented

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by Patryk Wierzyński
+ */
+
+public class ServerLoadBalancer {
+
+	public void balance(Server[] servers, Vm[] vms) {
+
+	}
+}
---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -0,0 +1,26 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Created by Patryk Wierzyński
+ */
+
+public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+	private double expectedLoadPercentage;
+
+	public CurrentLoadPercentageMatcher(double expectedLoadPercentage) {
+
+		this.expectedLoadPercentage = expectedLoadPercentage;
+	}
+
+	protected boolean matchesSafely(Server server) {
+		return expectedLoadPercentage == server.currentLoadPercentage
+				|| Math.abs(expectedLoadPercentage - server.currentLoadPercentage) < 0.01d;
+	}
+
+	public void describeTo(Description description) {
+
+	}
+}
