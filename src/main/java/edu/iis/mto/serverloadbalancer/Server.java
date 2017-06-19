ec17836ad1f49af108a00d7b6ed5e22ce6ca9f2e
Patryk Wierzyński <p.wierzynski@live.com>
2017-06-20 01:24:49 +0200

    test3 - refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -6,10 +6,15 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Server {
 
+	public static final double MAX_LOAD = 100.0d;
 	public double currentLoadPercentage;
 	public int capacity;
 
 	public Server(int capacity) {
 		this.capacity = capacity;
+	}
+
+	public void addVm(Vm vm) {
+		currentLoadPercentage = ((double) vm.size / capacity) * MAX_LOAD;
 	}
 }
